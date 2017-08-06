49bad55320f08bc21142ed1fe641d17b15d84c8c
linhho.net <homautuanlinh@gmail.com>
2017-08-06 20:47:05 +0700

    Vglobal header, banner and mobile side-bar

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    let sideBar = document.getElementById('side-bar');
+    sideBar.style.left = 0;
+}
+function hideMenu() {
+    let sideBar = document.getElementById('side-bar');
+    sideBar.style.left = '-260px';
+}
