a57a46bb2ae0e016347cf8b2d1fc220fccdc583e
linhho.net <homautuanlinh@gmail.com>
2017-08-06 22:54:12 +0700

    drop-box lang

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function hideMenu() {
     let sideBar = document.getElementById('side-bar');
     sideBar.style.left = '-260px';
 }
+function showDropBox() {
+    let lang = document.getElementById('lang-drop-box');
+    if(lang.clientHeight == 0){
+        lang.style.height = 'auto';
+        lang.style.border = '1px solid #616161';
+    }
+    else {
+        lang.style.height = 0;
+        lang.style.border = '0px transparent';
+    }
+}
